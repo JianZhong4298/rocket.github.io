87bae4be6c5719f555e764f879ba04da937c297e
DESKTOP-TDODD4K\Think <429855409@qq.com>
2023-05-23 23:32:45 +0800

    'commit'

--- js/public.js
+++ js/public.js
@@ -95,7 +95,7 @@ var getValueFromSelect = function(dom) {
 
 //获取服务器地址
 var getBaseUrl = function() {
-    return 'http://rocket.dev.yes-z.com'; 
+    return 'https://rocket.dev.yes-z.com'; 
 }
 
 //如果当前是登录状态，阻止返回登录页面
